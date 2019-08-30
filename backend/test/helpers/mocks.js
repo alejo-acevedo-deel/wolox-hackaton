a16eeb6690de217449a74d832395296eb57acefc
alejoacevedo <ale.acevedo@live.com.ar>
2019-08-30 17:43:13 -0300

    Delete folder

--- backend/test/helpers/mocks.js
+++
@@ -1,4 +0,0 @@
-const command = require('../../generators/app/command');
-
-exports.mockCommand = (implementation = () => Promise.resolve()) =>
-  jest.spyOn(command, 'runCommand').mockImplementation(implementation);
