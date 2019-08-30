a1dba2e97ef2e82a15b562612afc0db52e38f68a
alejoacevedo <ale.acevedo@live.com.ar>
2019-08-30 20:40:29 -0300

    Add presets models, controllers and services

---
+++ backen-hub/app/models/presets.js
@@ -0,0 +1,79 @@
+module.exports = (sequelize, DataTypes) => {
+  const Preset = sequelize.define(
+    'presets',
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      startHour: {
+        type: DataTypes.String,
+        allowNull: false,
+        field: 'start_hour'
+      },
+      startMinute: {
+        type: DataTypes.String,
+        allowNull: false,
+        field: 'start_minute'
+      },
+      endHour: {
+        type: DataTypes.String,
+        allowNull: false,
+        field: 'end_hour'
+      },
+      endMinute: {
+        type: DataTypes.String,
+        allowNull: false,
+        field: 'end_minute'
+      },
+      level: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      modIn: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'mod_in'
+      },
+      modOut: {
+        type: DataTypes.String,
+        allowNull: false,
+        field: 'mod_out'
+      },
+      // Ejecuta siempre a la hora de inicio
+      actionStart: {
+        type: DataTypes.String,
+        allowNull: false,
+        field: 'action_end'
+      },
+      // Ejecuta siempre a la hora de fin
+      actionEnd: {
+        type: DataTypes.String,
+        allowNull: false,
+        field: 'action_start'
+      },
+      // Ejecuta siempre si el level supera al seteado
+      actionOn: {
+        type: DataTypes.String,
+        allowNull: false,
+        field: 'action_on'
+      },
+      // Ejecuta siempre si el level esta por debajo del seteado
+      actionOff: {
+        type: DataTypes.String,
+        allowNull: false,
+        field: 'action_off'
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        field: 'created_at'
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: 'updated_at'
+      }
+    },
+    {}
+  );
+  return Preset;
+};
---
+++ backen-hub/app/services/presets.js
@@ -0,0 +1,30 @@
+const { Preset } = require('../models');
+const { databaseError, badRequestError } = require('../errors');
+
+exports.createOne = body =>
+  Preset.create({
+    name: body.name,
+    startHour: body.startHour,
+    startMinute: body.startMinute,
+    endHour: body.endHour,
+    endMinute: body.endMinute,
+    level: body.level,
+    modIn: body.modIn,
+    modOut: body.modOut,
+    actionStart: body.actionStart,
+    actionEnd: body.actionEnd,
+    actionOn: body.actionOn,
+    actionOff: body.actionOff
+  }).catch(err => {
+    if (err.message === 'Validation error') throw badRequestError('Unique key alredy exist');
+  });
+
+exports.getAll = () =>
+  Preset.getAll().catch(err => {
+    throw databaseError(err.message);
+  });
+
+exports.deleteOne = id =>
+  Preset.destroy({ where: { id } }).catch(err => {
+    throw databaseError(err.message);
+  });
